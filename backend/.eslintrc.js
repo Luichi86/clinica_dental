0dbcf1fc2dbdc5f383705077950ac622def302b6
martin <msrroschi@gmail.com>
2021-02-25 18:41:59 +0000

    Configure eslint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:prettier/recommended', 'eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+}
